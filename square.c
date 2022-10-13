9952042453f601f6829b304485959ef9d5362320
HajarElansary <hajarbe2016@gmail.com>
2022-10-13 16:21:29 +0200

    q9

---
+++ square.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+
+int affichage(){
+	printf("Le début du programme\n");
+}
+
+int square(int n) { 
+   return n*n; 
+}
+
+/*
+int main()
+{
+  int x;
+  affichage();
+  x = square(5);
+  printf("Le carré est : %d\n", x );
+}
+*/
